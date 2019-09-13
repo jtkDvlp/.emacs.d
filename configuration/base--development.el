8de7ec30decce4a81a460d13a67164415c854931
jtkDvlp <jtkdevelopments@gmail.com>
2019-09-13 16:22:52 +0200

    Adds markdown mode

--- configuration/base--development.el
+++ configuration/base--development.el
@@ -123,6 +123,14 @@
     (setq sql-mysql-options '("-C" "-t" "-f" "-n"))))
 
 (use-package
+  markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(use-package
   pkg--ide
   :load-path "configuration/"
   :config
