29b53a04e15d27e7367594133ca9fcdfb5ac6d3d
jtkDvlp <jtkdevelopments@gmail.com>
2020-12-18 09:09:07 +0100

    Improves c++ integration

--- configuration/lang--c.el
+++ configuration/lang--c.el
@@ -5,10 +5,18 @@
   :mode
   (("\\.ino\\'" . c++-mode)
    ("\\.h\\'" . c++-mode)
-   ("\\.cpp\\'" . c++-mode)))
+   ("\\.cpp\\'" . c++-mode))
+
+  :hook
+  ((c++-mode . (lambda () (rainbow-mode 0)))))
 
 (use-package
   platformio-mode
-  :hook (c++-mode . platformio-mode))
+
+  :mode
+  (("platformio\\.ini\\'" . platformio-mode))
+
+  :hook
+  ((c++-mode . platformio-mode)))
 
 (provide 'lang--c)
